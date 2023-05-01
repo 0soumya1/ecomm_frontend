d60bb4aa2b2532908acd6a82baeab320eb015b69
Soumya <soumyaguptabond@gamil.com>
2023-05-01 18:37:21 +0530

    first commit

---
+++ src/Components/AddProduct.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+
+const AddProduct = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [company, setCompany] = useState("");
+  const [error, setError] = useState(false);
+
+  const addProduct = async () => {
+    
+    console.log(!name);
+    if (!name || !price || !category || !company) {
+      setError(true);
+      return false;
+    }
+
+    console.log(name, price, category, company);
+    const userData = JSON.parse(localStorage.getItem("user"));
+   
+    // let obj = {             
+    //   "key": value,
+    //   key2: value2,
+    // };
+//array of objects
+    // let arr = [
+    //   { key: value, key2: value2 },
+    //   { key: value, key2: value2 },
+    // ];
+
+    let data = {
+      name: name,
+      price: price,
+      category: category,
+      company: company,
+      userID: userData._id,
+    };
+
+    let result = await fetch("http://localhost:5000/add-product", {
+      method: "post",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        authorization:`bearer ${JSON.parse(localStorage.getItem("token"))}`
+      },
+    });
+    result = await result.json();
+    console.log(result);
+  };
+
+  return (
+    <div className="product">
+      <h1>Add Product</h1>
+      <input
+        type="text"
+        placeholder="Enter Product Name"
+        className="inputbox"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+      />
+      {error && !name && (
+        <span className="invalid-input">Enter valid name</span>
+      )}
+
+      <input
+        type="text"
+        placeholder="Enter Product Price"
+        className="inputbox"
+        onChange={(e) => setPrice(e.target.value)}
+        value={price}
+      />
+      {error && !price && (
+        <span className="invalid-input">Enter valid price</span>
+      )}
+
+      <input
+        type="text"
+        placeholder="Enter Product Category"
+        className="inputbox"
+        onChange={(e) => setCategory(e.target.value)}
+        value={category}
+      />
+      {error && !category && (
+        <span className="invalid-input">Enter valid category</span>
+      )}
+
+      <input
+        type="text"
+        placeholder="Enter Product Company"
+        className="inputbox"
+        onChange={(e) => setCompany(e.target.value)}
+        value={company}
+      />
+      {error && !company && (
+        <span className="invalid-input">Enter valid company</span>
+      )}
+
+      <button onClick={addProduct} className="appbutton">
+        Add Product
+      </button>
+    </div>
+  );
+};
+
+export default AddProduct;
---
+++ src/Components/SignUp.js
@@ -0,0 +1,50 @@
+import React, {useState, useEffect} from "react";
+import {useNavigate} from "react-router-dom";
+
+const SignUp =()=>{
+    const[name, setName]= useState("");
+    const[email, setEmail]= useState("");
+    const[password, setPassword]= useState("");
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        const auth = localStorage.getItem('user');
+        if(auth){
+            navigate('/')
+        } 
+    }, [])
+    
+    const collectData= async()=>{
+        console.log(name,email,password); 
+        let result = await fetch('http://localhost:5000/register', {
+            method:'post',
+            body: JSON.stringify({name,email,password}),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        });
+        result = await result.json()
+        console.log(result, "resullt");
+        localStorage.setItem('user', JSON.stringify(result.result));
+        localStorage.setItem('token', JSON.stringify(result.auth));
+        navigate('/')
+    }
+
+    return(
+        <div className="register">
+            <h1>Register </h1>
+            <input className="inputbox" type="text" placeholder="Enter Name"
+             value={name} onChange={(e)=>setName(e.target.value)} ></input>
+
+            <input className="inputbox" type="text" placeholder="Enter Email"
+            value={email} onChange={(e)=>setEmail(e.target.value)} ></input>
+
+            <input className="inputbox" type="password" placeholder="Enter Password"
+            value={password} onChange={(e)=>setPassword(e.target.value)}></input>
+
+            <button onClick={collectData} className="appbutton" type="button">SignUp</button>
+        </div>
+    )
+}
+
+export default SignUp;
